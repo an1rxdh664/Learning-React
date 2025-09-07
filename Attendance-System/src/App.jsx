a840abb7ad02cd11458e89aab7a3ea9eccbfb069
an1rxdh664 <anirudhhh637@gmail.com>
2025-09-08 00:13:57 +0530

    initial commit

---
+++ Attendance-System/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import Food from "./Food"
+
+function App() {
+  return(
+    <>
+      
+    </>
+    // in react you cannot return multiple components so you have to use empty angle brackets to return them.
+  );
+}
+
+export default App
---
+++ Attendance-System/src/Header.jsx
@@ -0,0 +1,24 @@
+function Header(){
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li>
+                        Bello
+                    </li>
+                    <li>
+                        Blehh
+                    </li>
+                    <li>
+                        Bluhhh
+                    </li>
+                    <li>
+                        Blaahhh
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+export default Header
